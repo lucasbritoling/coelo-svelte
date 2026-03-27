0fb482affd6ff9b1bc53a2a4dea4512aa422bc24
Lucas <lucasbritoling@gmail.com>
2026-03-27 17:14:01 -0300

    feat(ui): navuser metadata & signOut form action

--- src/lib/components/nav-user.svelte
+++ src/lib/components/nav-user.svelte
@@ -10,79 +10,94 @@
 	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
 	import { useSidebar } from '$lib/components/ui/sidebar/index.js';
+	import { page } from '$app/state';
 
-	let { user }: { user: { name: string; email: string; avatar: string } } = $props();
+	let { ...restProps } = $props();
 	const sidebar = useSidebar();
+	const user = $derived(page.data.user);
 </script>
 
+<form action="/logout" method="POST" id="logout-form" style="display: none;"></form>
+
 <Sidebar.Menu>
-	<Sidebar.MenuItem>
-		<DropdownMenu.Root>
-			<DropdownMenu.Trigger>
-				{#snippet child({ props })}
-					<Sidebar.MenuButton
-						{...props}
-						size="lg"
-						class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+	{#if user}
+		<Sidebar.MenuItem>
+			<DropdownMenu.Root>
+				<DropdownMenu.Trigger>
+					{#snippet child({ props })}
+						<Sidebar.MenuButton
+							{...props}
+							size="lg"
+							class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+						>
+							<Avatar.Root class="size-8 rounded-lg">
+								<Avatar.Image src={user.avatar} alt={user.full_name} />
+								<Avatar.Fallback class="rounded-lg">
+									{user.full_name.slice(0, 2).toUpperCase()}
+								</Avatar.Fallback>
+							</Avatar.Root>
+							<div class="grid flex-1 text-start text-sm leading-tight">
+								<span class="truncate font-medium">{user.full_name}</span>
+								<span class="truncate text-xs">{user.email}</span>
+							</div>
+							<ChevronsUpDownIcon class="ms-auto size-4" />
+						</Sidebar.MenuButton>
+					{/snippet}
+				</DropdownMenu.Trigger>
+				<DropdownMenu.Content
+					class="w-(--bits-dropdown-menu-anchor-width) min-w-56 rounded-lg"
+					side={sidebar.isMobile ? 'bottom' : 'right'}
+					align="start"
+					sideOffset={4}
+				>
+					<DropdownMenu.Label class="p-0 font-normal">
+						<div class="flex items-center gap-2 px-1 py-1.5 text-start text-sm">
+							<Avatar.Root class="size-8 rounded-lg">
+								<Avatar.Image src={user.avatar} alt={user.full_name} />
+								<Avatar.Fallback class="rounded-lg"
+									>{user.full_name.slice(0, 2).toUpperCase()}</Avatar.Fallback
+								>
+							</Avatar.Root>
+							<div class="grid flex-1 text-start text-sm leading-tight">
+								<span class="truncate font-medium">{user.full_name}</span>
+								<span class="truncate text-xs">{user.email}</span>
+							</div>
+						</div>
+					</DropdownMenu.Label>
+					<DropdownMenu.Separator />
+					<DropdownMenu.Group>
+						<DropdownMenu.Item>
+							<SparklesIcon />
+							Seja Pro
+						</DropdownMenu.Item>
+					</DropdownMenu.Group>
+					<DropdownMenu.Separator />
+					<DropdownMenu.Group>
+						<DropdownMenu.Item>
+							<BadgeCheckIcon />
+							Conta
+						</DropdownMenu.Item>
+						<DropdownMenu.Item>
+							<CreditCardIcon />
+							Assinatura
+						</DropdownMenu.Item>
+						<DropdownMenu.Item>
+							<BellIcon />
+							Notificações
+						</DropdownMenu.Item>
+					</DropdownMenu.Group>
+					<DropdownMenu.Separator />
+					<DropdownMenu.Item
+						onSelect={() => {
+							const form = document.getElementById('logout-form') as HTMLFormElement;
+							form?.submit();
+						}}
 					>
-						<Avatar.Root class="size-8 rounded-lg">
-							<Avatar.Image src={user.avatar} alt={user.name} />
-							<Avatar.Fallback class="rounded-lg">CN</Avatar.Fallback>
-						</Avatar.Root>
-						<div class="grid flex-1 text-start text-sm leading-tight">
-							<span class="truncate font-medium">{user.name}</span>
-							<span class="truncate text-xs">{user.email}</span>
-						</div>
-						<ChevronsUpDownIcon class="ms-auto size-4" />
-					</Sidebar.MenuButton>
-				{/snippet}
-			</DropdownMenu.Trigger>
-			<DropdownMenu.Content
-				class="w-(--bits-dropdown-menu-anchor-width) min-w-56 rounded-lg"
-				side={sidebar.isMobile ? 'bottom' : 'right'}
-				align="start"
-				sideOffset={4}
-			>
-				<DropdownMenu.Label class="p-0 font-normal">
-					<div class="flex items-center gap-2 px-1 py-1.5 text-start text-sm">
-						<Avatar.Root class="size-8 rounded-lg">
-							<Avatar.Image src={user.avatar} alt={user.name} />
-							<Avatar.Fallback class="rounded-lg">CN</Avatar.Fallback>
-						</Avatar.Root>
-						<div class="grid flex-1 text-start text-sm leading-tight">
-							<span class="truncate font-medium">{user.name}</span>
-							<span class="truncate text-xs">{user.email}</span>
-						</div>
-					</div>
-				</DropdownMenu.Label>
-				<DropdownMenu.Separator />
-				<DropdownMenu.Group>
-					<DropdownMenu.Item>
-						<SparklesIcon />
-						Seja Pro
+						<LogOutIcon />
+						Sair
 					</DropdownMenu.Item>
-				</DropdownMenu.Group>
-				<DropdownMenu.Separator />
-				<DropdownMenu.Group>
-					<DropdownMenu.Item>
-						<BadgeCheckIcon />
-						Conta
-					</DropdownMenu.Item>
-					<DropdownMenu.Item>
-						<CreditCardIcon />
-						Assinatura
-					</DropdownMenu.Item>
-					<DropdownMenu.Item>
-						<BellIcon />
-						Notificações
-					</DropdownMenu.Item>
-				</DropdownMenu.Group>
-				<DropdownMenu.Separator />
-				<DropdownMenu.Item>
-					<LogOutIcon />
-					Sair
-				</DropdownMenu.Item>
-			</DropdownMenu.Content>
-		</DropdownMenu.Root>
-	</Sidebar.MenuItem>
+				</DropdownMenu.Content>
+			</DropdownMenu.Root>
+		</Sidebar.MenuItem>
+	{/if}
 </Sidebar.Menu>

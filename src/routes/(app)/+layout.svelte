6a4393dd3bfb1628b7ab693e21985b679576373b
Lucas <lucasbritoling@gmail.com>
2026-03-27 14:18:26 -0300

    install shadcn blocks signup-03 & sidebar-12

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,1 +1,30 @@
-.
+<script lang="ts">
+	import AppSidebar from '$lib/components/app-sidebar.svelte';
+	import * as Breadcrumb from '$lib/components/ui/breadcrumb/index.js';
+	import { Separator } from '$lib/components/ui/separator/index.js';
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+</script>
+
+<Sidebar.Provider>
+	<AppSidebar />
+	<Sidebar.Inset>
+		<header class="sticky top-0 flex h-16 shrink-0 items-center gap-2 border-b bg-background px-4">
+			<Sidebar.Trigger class="-ms-1" />
+			<Separator orientation="vertical" class="me-2 data-[orientation=vertical]:h-4" />
+			<Breadcrumb.Root>
+				<Breadcrumb.List>
+					<Breadcrumb.Item>
+						<Breadcrumb.Page>October 2024</Breadcrumb.Page>
+					</Breadcrumb.Item>
+				</Breadcrumb.List>
+			</Breadcrumb.Root>
+		</header>
+		<div class="flex flex-1 flex-col gap-4 p-4">
+			<div class="grid auto-rows-min gap-4 md:grid-cols-5">
+				{#each Array.from({ length: 20 }) as _, index (index)}
+					<div class="aspect-square rounded-xl bg-muted/50"></div>
+				{/each}
+			</div>
+		</div>
+	</Sidebar.Inset>
+</Sidebar.Provider>

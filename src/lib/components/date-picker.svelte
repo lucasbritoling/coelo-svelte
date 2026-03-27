7223ffd6e1f9c63b53cdeccfa18403daec4f4f97
Lucas <lucasbritoling@gmail.com>
2026-03-27 16:20:38 -0300

    ui(calendar)

--- src/lib/components/date-picker.svelte
+++ src/lib/components/date-picker.svelte
@@ -4,23 +4,25 @@
 	import type { DateValue } from '@internationalized/date';
 
 	// Definimos as props para comunicação com o AppSidebar
-    let { 
-        value = $bindable(), 
-        onValueChange 
-    }: { 
-        value: DateValue | undefined, 
-        onValueChange?: (d: DateValue | undefined) => void 
-    } = $props();
-
+	let {
+		value = $bindable(),
+		onValueChange
+	}: {
+		value: DateValue | undefined;
+		onValueChange?: (d: DateValue | undefined) => void;
+	} = $props();
 </script>
 
 <Sidebar.Group class="px-0">
 	<Sidebar.GroupContent>
 		<Calendar
 			bind:value={value as any}
-            onValueChange={onValueChange}
+			{onValueChange}
 			type="single"
-			class="select-none [&_[data-bits-calendar-head-cell]]:w-[33px] [&_[role=gridcell]]:w-[33px] [&_[role=gridcell]_[role=button][data-today]]:bg-sidebar-primary [&_[role=gridcell]_[role=button][data-today]]:text-sidebar-primary-foreground"
+			class="select-none [&_[data-bits-calendar-head-cell]]:w-[33px] [&_[role=gridcell]]:w-[33px] [&_[role=gridcell]_[data-selected]]:bg-sidebar-primary [&_[role=gridcell]_[data-selected]]:text-sidebar-primary-foreground
+    [&_[role=gridcell]_[data-today]:not([data-selected])]:bg-sidebar-accent
+    [&_[role=gridcell]_[data-today]:not([data-selected])]:font-bold
+    [&_[role=gridcell]_[data-today]:not([data-selected])]:text-sidebar-accent-foreground"
 		/>
 	</Sidebar.GroupContent>
 </Sidebar.Group>

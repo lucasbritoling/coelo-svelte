4bf8ec579a936f3fedb6b359999bd082d3a30415
Lucas <lucasbritoling@gmail.com>
2026-04-05 17:45:54 -0300

    fix ui

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -20,7 +20,7 @@
 <Sidebar.Provider>
 	<AppSidebar />
 	<Sidebar.Inset class="min-w-0 overflow-hidden">
-		<header class="sticky top-0 flex h-16 shrink-0 items-center gap-2 border-b bg-background px-4">
+		<header class="sticky! top-0 flex h-16 shrink-0 items-center gap-2 border-b bg-background px-4">
 			<Sidebar.Trigger class="-ms-1" />
 			<Separator orientation="vertical" class="me-2 data-[orientation=vertical]:h-4" />
 			<Breadcrumb.Root>
@@ -31,7 +31,7 @@
 				</Breadcrumb.List>
 			</Breadcrumb.Root>
 		</header>
-		<main class="flex min-w-0 flex-1 flex-col gap-4 p-4">
+		<main class="flex min-w-0! sm:items-center flex-1 flex-col gap-4 p-4">
 			{@render children()}
 		</main>
 	</Sidebar.Inset>

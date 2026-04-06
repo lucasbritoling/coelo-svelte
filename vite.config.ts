26b3d6e720fa47c4d5ccf625165fc1816c41122c
Lucas <lucasbritoling@gmail.com>
2026-04-06 18:00:46 -0300

    chore

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { SvelteKitPWA } from '@vite-pwa/sveltekit';
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [
@@ -9,23 +9,29 @@ export default defineConfig({
 		sveltekit(),
 		SvelteKitPWA({
 			registerType: 'autoUpdate',
-			injectRegister: 'auto',
 			manifest: {
 				name: 'Coelo',
 				short_name: 'Coelo',
+				display: 'standalone',
+				start_url: '/',
 				id: '/',
+				scope: '/',
+				description: 'Link de agendamento para profissionais',
 				theme_color: '#ffffff',
 				background_color: '#ffffff',
-				display: 'standalone',
 				icons: [
-					{ src: 'icon-192.png', sizes: '192x192', type: 'image/png' },
-					{ src: 'icon-512.png', sizes: '512x512', type: 'image/png' }
+					{
+						src: 'icon-192.png',
+						sizes: '192x192',
+						type: 'image/png'
+					},
+					{
+						src: 'icon-512.png',
+						sizes: '512x512',
+						type: 'image/png'
+					}
 				]
 			}
 		})
-	],
-	test: {
-		// Isso diz ao Vitest para procurar arquivos .test.ts em qualquer lugar na pasta src
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+	]
 });

6f35364221b4671fad42ae74396d7f8c6f377b0e
Lucas <lucasbritoling@gmail.com>
2026-04-06 14:24:29 -0300

    PWA CONFIGURATION

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,7 @@ import adapter from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: { adapter: adapter() },
+	kit: { adapter: adapter(), serviceWorker: { register: false } },
 	vitePlugin: {
 		dynamicCompileOptions: ({ filename }) =>
 			filename.includes('node_modules') ? undefined : { runes: true }
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,27 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
+	plugins: [
+		tailwindcss(),
+		sveltekit(),
+		SvelteKitPWA({
+			registerType: 'autoUpdate',
+			manifest: {
+				name: 'Coelo',
+				short_name: 'Coelo',
+				theme_color: '#ffffff',
+				background_color: '#ffffff',
+				display: 'standalone',
+				icons: [
+					{ src: 'icon-192.png', sizes: '192x192', type: 'image/png' },
+					{ src: 'icon-512.png', sizes: '512x512', type: 'image/png' }
+				]
+			}
+		})
+	],
 	test: {
 		// Isso diz ao Vitest para procurar arquivos .test.ts em qualquer lugar na pasta src
 		include: ['src/**/*.{test,spec}.{js,ts}']

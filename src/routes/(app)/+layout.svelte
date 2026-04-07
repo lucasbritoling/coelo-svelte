e138aea9101a8ab639a64c79cbb2d03b1e32f32c
Lucas <lucasbritoling@gmail.com>
2026-04-07 18:09:04 -0300

    ui: background color consistency

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -21,7 +21,7 @@
 	<AppSidebar />
 	<Sidebar.Inset class="flex h-svh flex-col overflow-hidden">
 		<header
-			class="sticky! bottom-0! z-20 order-last flex h-16 shrink-0 items-center gap-2 border-t bg-muted px-4 shadow-[0_-4px_12px_rgba(0,0,0,0.1)] sm:top-0! sm:bottom-auto! sm:order-0 sm:border-t-0 sm:border-b sm:bg-background sm:shadow-none dark:shadow-[0_-4px_12px_rgba(0,0,0,0.3)]"
+			class="sticky! bottom-0! z-20 order-last flex h-16 shrink-0 items-center gap-2 border-t bg-zinc-50! px-4 shadow-[0_-4px_12px_rgba(0,0,0,0.1)] sm:top-0! sm:bottom-auto! sm:order-0 sm:border-t-0 sm:border-b sm:bg-background sm:shadow-none dark:shadow-[0_-4px_12px_rgba(0,0,0,0.3)]"
 		>
 			<Sidebar.Trigger class="order-last -me-1 sm:order-0 sm:-ms-1 sm:me-0" />
 			<Separator
@@ -37,7 +37,7 @@
 			</Breadcrumb.Root>
 		</header>
 		<main
-			class="order-first flex min-w-0! flex-1 flex-col gap-4 overflow-y-auto p-4 sm:order-0 sm:items-center"
+			class="order-first flex min-w-0! flex-1 flex-col gap-4 overflow-y-auto bg-zinc-50! p-4 sm:order-0 sm:items-center"
 		>
 			{@render children()}
 		</main>

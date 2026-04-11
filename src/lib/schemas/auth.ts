e67918c033188f83f595262d6b60214a9c7170a9
Lucas <lucasbritoling@gmail.com>
2026-04-11 15:20:45 -0300

    schemas: minor

--- src/lib/schemas/auth.ts
+++ src/lib/schemas/auth.ts
@@ -108,7 +108,7 @@ export const signupSchema = z
 	.object({
 		full_name: z.string().min(3, 'Muito curto'),
 		email: z.email('E-mail inválido'),
-		password: z.string().min(6, 'Muito curto'),
+		password: z.string().min(6, 'Mínimo 6 dígitos'),
 		confirmPassword: z.string(),
 		username: usernameSchema
 	})

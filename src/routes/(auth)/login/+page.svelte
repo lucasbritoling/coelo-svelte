8aa4fa00cc85ceba6115416f96e936a5bc572753
Lucas <lucasbritoling@gmail.com>
2026-04-11 14:54:47 -0300

    ui(auth): logos on signup & login pages

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import LoginForm from '$lib/components/login-form.svelte';
-	import Rabbit from '@lucide/svelte/icons/rabbit';
 
 	let { form } = $props();
 </script>
@@ -8,10 +7,8 @@
 <div class="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
 	<div class="flex w-full max-w-sm flex-col gap-6">
 		<a href="##" class="flex items-center gap-2 self-center font-medium">
-			<div
-				class="flex size-6 items-center justify-center rounded-md bg-primary text-primary-foreground"
-			>
-				<Rabbit class="size-4" />
+			<div class="flex size-6 items-center justify-center overflow-hidden rounded-md bg-primary">
+				<img src="/icon-192.png" alt="Coelo Logo" class="size-full object-cover" />
 			</div>
 			Coelo
 		</a>
--- src/routes/(auth)/signup/+page.svelte
+++ src/routes/(auth)/signup/+page.svelte
@@ -1,16 +1,13 @@
 <script>
-	import { Rabbit } from '@lucide/svelte';
 	import SignupForm from '$lib/components/signup-form.svelte';
 	let { data } = $props();
 </script>
 
 <div class="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
 	<div class="flex w-full max-w-sm flex-col gap-6">
-		<a href="#/" class="flex items-center gap-2 self-center font-medium">
-			<div
-				class="flex size-6 items-center justify-center rounded-md bg-primary text-primary-foreground"
-			>
-				<Rabbit class="size-4" />
+		<a href="##" class="flex items-center gap-2 self-center font-medium">
+			<div class="flex size-6 items-center justify-center overflow-hidden rounded-md bg-primary">
+				<img src="/icon-192.png" alt="Coelo Logo" class="size-full object-cover" />
 			</div>
 			Coelo
 		</a>

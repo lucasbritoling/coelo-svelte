2f9c176760c64966788f546c7284e2ee87dd53eb
Lucas <lucasbritoling@gmail.com>
2026-04-15 17:45:44 -0300

    CSP: hot

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,23 @@ import adapter from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: { adapter: adapter(), serviceWorker: { register: false } },
+	kit: {
+		adapter: adapter(),
+		serviceWorker: { register: false },
+		csp: {
+			directives: {
+				'default-src': ['self'],
+				'script-src': ['self', 'https://static.cloudflareinsights.com'],
+				'style-src': ['self', 'unsafe-inline'],
+				'img-src': ['self', 'data:'],
+				'font-src': ['self'],
+				'connect-src': ['self', 'https://cloudflareinsights.com'],
+				'object-src': ['none'],
+				'base-uri': ['self'],
+				'frame-ancestors': ['none']
+			}
+		}
+	},
 	vitePlugin: {
 		dynamicCompileOptions: ({ filename }) =>
 			filename.includes('node_modules') ? undefined : { runes: true }

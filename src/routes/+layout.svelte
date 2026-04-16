34e0f7ccde9b1493cf487b603ee5ee2b91d1fff4
Lucas <lucasbritoling@gmail.com>
2026-04-15 22:28:45 -0300

    seo upgrade 2

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,4 +4,9 @@
 	let { children } = $props();
 </script>
 
+<svelte:head>
+	<title>Coelo</title>
+	<meta name="description" content="Link de autoagendamento. Rápido. Simples." />
+</svelte:head>
+
 {@render children()}

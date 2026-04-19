3ad43359d054505601bd988e3213f5fed463739b
Lucas <lucasbritoling@gmail.com>
2026-04-18 22:49:53 -0300

    format

--- vite.config.ts
+++ vite.config.ts
@@ -9,43 +9,44 @@ export default defineConfig({
 		sveltekit(),
 		SvelteKitPWA({
 			registerType: 'autoUpdate',
-  workbox: { runtimeCaching: [
-    {
-      // 1. Cache Agressivo (Ex: Página de Serviços ou Horários Fixos)
-      // Perfeito para o que quase nunca muda. Tenta o cache e NEM OLHA para a rede.
-      urlPattern: /\/(servicos|horarios)/,
-      handler: 'CacheFirst', 
-      options: {
-        cacheName: 'paginas-estaticas',
-        expiration: {
-          maxEntries: 10,
-          maxAgeSeconds: 60 * 60 * 24 * 30, // Mantém por 30 dias
-        },
-      },
-    },
-    {
-      // 2. Cache Inteligente (Ex: Agenda ou Perfil do Usuário)
-      // Mostra o que tem no cache instantaneamente, mas atualiza em segundo plano.
-      urlPattern: /\/(agenda|clientes|[\w-]+)/, 
-      handler: 'StaleWhileRevalidate',
-      options: {
-        cacheName: 'dados-dinamicos',
-      },
-    }
-  ],
-                // 1. Força o cache de todos os arquivos gerados e da pasta static
-                globPatterns: ['**/*.{js,css,html,ico,png,svg,webmanifest}'],
-                
-                // 2. Garante que o app funcione como Single Page App (SPA) offline
-                navigateFallback: '/',
-                
-                // 3. Limpa caches de versões antigas automaticamente
-                cleanupOutdatedCaches: true,
+			workbox: {
+				runtimeCaching: [
+					{
+						// 1. Cache Agressivo (Ex: Página de Serviços ou Horários Fixos)
+						// Perfeito para o que quase nunca muda. Tenta o cache e NEM OLHA para a rede.
+						urlPattern: /\/(servicos|horarios)/,
+						handler: 'CacheFirst',
+						options: {
+							cacheName: 'paginas-estaticas',
+							expiration: {
+								maxEntries: 10,
+								maxAgeSeconds: 60 * 60 * 24 * 30 // Mantém por 30 dias
+							}
+						}
+					},
+					{
+						// 2. Cache Inteligente (Ex: Agenda ou Perfil do Usuário)
+						// Mostra o que tem no cache instantaneamente, mas atualiza em segundo plano.
+						urlPattern: /\/(agenda|clientes|[\w-]+)/,
+						handler: 'StaleWhileRevalidate',
+						options: {
+							cacheName: 'dados-dinamicos'
+						}
+					}
+				],
+				// 1. Força o cache de todos os arquivos gerados e da pasta static
+				globPatterns: ['**/*.{js,css,html,ico,png,svg,webmanifest}'],
 
-                // 4. Faz o Service Worker assumir o controle do app na hora
-                skipWaiting: true,
-                clientsClaim: true
-            },
+				// 2. Garante que o app funcione como Single Page App (SPA) offline
+				navigateFallback: '/',
+
+				// 3. Limpa caches de versões antigas automaticamente
+				cleanupOutdatedCaches: true,
+
+				// 4. Faz o Service Worker assumir o controle do app na hora
+				skipWaiting: true,
+				clientsClaim: true
+			},
 			manifest: {
 				name: 'Coelo',
 				short_name: 'Coelo',

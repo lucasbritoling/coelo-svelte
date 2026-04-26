b02085999a7ea0a8bb684b4d0705894684a7ba21
Lucas B. Ling <lucasbritoling@gmail.com>
2026-04-26 17:06:08 -0300

    pwa atalho glorificado

--- vite.config.ts
+++ vite.config.ts
@@ -10,49 +10,14 @@ export default defineConfig({
 		SvelteKitPWA({
 			registerType: 'autoUpdate',
 			workbox: {
-				runtimeCaching: [
-					{
-						// 1. Cache Agressivo (Ex: Página de Serviços ou Horários Fixos)
-						// Perfeito para o que quase nunca muda. Tenta o cache e NEM OLHA para a rede.
-						urlPattern: /\/(servicos|horarios)/,
-						handler: 'CacheFirst',
-						options: {
-							cacheName: 'paginas-estaticas',
-							expiration: {
-								maxEntries: 10,
-								maxAgeSeconds: 60 * 60 * 24 * 30 // Mantém por 30 dias
-							}
-						}
-					},
-					{
-						// 2. Cache Inteligente (Ex: Agenda ou Perfil do Usuário)
-						// Mostra o que tem no cache instantaneamente, mas atualiza em segundo plano.
-						urlPattern: /\/(agenda|clientes|[\w-]+)/,
-						handler: 'NetworkFirst',
-						options: {
-							cacheName: 'dados-dinamicos'
-						}
-					}
-				],
-				// 1. Força o cache de todos os arquivos gerados e da pasta static
-				globPatterns: ['**/*.{js,css,html,ico,png,svg,webmanifest}'],
-
-				// 2. Garante que o app funcione como Single Page App (SPA) offline
-				navigateFallback: '/',
-
-				// 3. Limpa caches de versões antigas automaticamente
-				cleanupOutdatedCaches: true,
-
-				// 4. Faz o Service Worker assumir o controle do app na hora
-				skipWaiting: true,
-				clientsClaim: true
+				globPatterns: []
 			},
 			manifest: {
 				name: 'Coelo',
 				short_name: 'Coelo',
 				display: 'standalone',
-				start_url: '/',
-				id: '/',
+				start_url: '/agenda',
+				id: '/agenda',
 				scope: '/',
 				description: 'Link de autoagendamento. Rápido. Simples.',
 				theme_color: '#191919',
@@ -68,13 +33,13 @@ export default defineConfig({
 						src: 'icon-512-rounded.png',
 						sizes: '512x512',
 						type: 'image/png',
-						purpose: 'any' // Desktop e Splash Screens (usa a transparência nativa)
+						purpose: 'any'
 					},
 					{
 						src: 'icon-512-squared.png',
 						sizes: '512x512',
 						type: 'image/png',
-						purpose: 'maskable' // Android (permite ao sistema cortar em círculo, gota ou quadrado)
+						purpose: 'maskable'
 					}
 				]
 			}

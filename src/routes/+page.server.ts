22168e7964b95676442e8c92f440bbb434e00c63
Lucas B. Ling <lucasbritoling@gmail.com>
2026-04-30 20:59:44 -0300

    minor

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,6 +2,10 @@
 import type { PageServerLoad } from './$types';
 import { redirect } from '@sveltejs/kit';
 
-export const load: PageServerLoad = async () => {
-	throw redirect(307, '/login');
+export const load: PageServerLoad = async ({ locals: { session } }) => {
+	if (session) {
+		throw redirect(303, '/agenda');
+	}
+
+	throw redirect(303, '/login');
 };

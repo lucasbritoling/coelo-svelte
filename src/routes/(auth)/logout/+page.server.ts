878e55f8bd843c8b77da65ab714e5b58d6ce0a4f
Lucas B. Ling <lucasbritoling@gmail.com>
2026-05-03 10:49:43 -0300

    fix: logout

--- src/routes/(auth)/logout/+page.server.ts
+++ src/routes/(auth)/logout/+page.server.ts
@@ -2,7 +2,10 @@ import { redirect } from '@sveltejs/kit';
 import type { Actions } from './$types';
 
 export const actions: Actions = {
-	default: async ({ locals: { supabase, session } }) => {
+	default: async ({ locals: { supabase } }) => {
+		const {
+			data: { session }
+		} = await supabase.auth.getSession();
 		if (session) {
 			await supabase.auth.signOut();
 		}

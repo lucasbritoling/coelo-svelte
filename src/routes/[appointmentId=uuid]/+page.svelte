71b983f9d57dfdf30c55b19343538f46376fee6f
Lucas B. Ling <lucasbritoling@gmail.com>
2026-05-09 14:17:29 -0300

    icon share

--- src/routes/[appointmentId=uuid]/+page.svelte
+++ src/routes/[appointmentId=uuid]/+page.svelte
@@ -47,7 +47,11 @@
 		property="og:description"
 		content="{appointment.service_name} com {professional.full_name} em {appointment.date} às {appointment.time}."
 	/>
-	<meta property="og:image" content="{page.url.origin}/icon-512-squared.png" />
+	<meta property="og:image" content="{page.url.origin}/icon-300-squared.png?v=1" />
+	<meta property="og:image:width" content="300" />
+	<meta property="og:image:height" content="300" />
+
+	<meta property="og:type" content="website" />
 	<meta name="twitter:card" content="summary" />
 </svelte:head>
 

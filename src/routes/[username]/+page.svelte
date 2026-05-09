475012f5edd74ff2b9d128576046b60aa84c88ef
Lucas B. Ling <lucasbritoling@gmail.com>
2026-05-09 12:30:38 -0300

    simpler selfbooking share

--- src/routes/[username]/+page.svelte
+++ src/routes/[username]/+page.svelte
@@ -92,29 +92,17 @@
 </script>
 
 <svelte:head>
-	<title>Agendamento com {professional.full_name}</title>
-	<meta
-		name="description"
-		content="Reserve seu horário com @{professional.username}. Rápido e simples."
-	/>
+	<title>Agende com {professional.full_name}</title>
+	<meta name="description" content="Reserve seu horário com @{professional.username}." />
 
 	<!-- Configuração de Compartilhamento (Open Graph) -->
 	<meta property="og:type" content="website" />
-	<meta property="og:title" content="Agendamento com {professional.full_name}" />
-	<meta
-		property="og:description"
-		content="Confira os horários disponíveis para {selectedService?.name ?? 'serviços'}."
-	/>
+	<meta property="og:title" content="Agende com {professional.full_name}" />
+	<meta property="og:description" content="Confira os horários disponíveis." />
 	<!-- Forçando o ícone do app -->
 	<meta property="og:image" content="/icon-512-squared.png" />
 	<meta property="og:image:width" content="512" />
 	<meta property="og:image:height" content="512" />
-
-	<!-- Twitter/X -->
-	<meta name="twitter:card" content="summary" />
-	<!-- 'summary' é melhor para ícones quadrados -->
-	<meta name="twitter:title" content="Agendamento com {professional.full_name}" />
-	<meta name="twitter:image" content="/icon-512-squared.png" />
 </svelte:head>
 
 <div class="mx-auto p-6 {data.singleService ? 'max-w-sm' : 'max-w-sm lg:max-w-5xl'}">

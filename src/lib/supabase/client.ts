16e0b718251d2ddf27cc43c020f6fac8185f5443
Lucas B. Ling <lucasbritoling@gmail.com>
2026-05-10 20:13:57 -0300

    remove unused supabase client file

--- src/lib/supabase/client.ts
+++
@@ -1,8 +0,0 @@
-import { createBrowserClient, isBrowser } from '@supabase/ssr';
-import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY } from '$env/static/public';
-
-// No navegador, criamos uma única instância (Singleton)
-// No servidor (SSR), o SvelteKit vai criar uma instância por requisição no hooks.server.ts
-export const supabase = isBrowser()
-	? createBrowserClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY)
-	: (null as any);

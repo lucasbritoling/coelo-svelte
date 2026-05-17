a4deef341106e279b630be4ba309bb7442e5eca7
Lucas Ling <lucasbritoling@gmail.com>
2026-05-17 18:50:02 -0300

    ok

--- src/lib/components/app/agenda/appointment-item.svelte
+++ src/lib/components/app/agenda/appointment-item.svelte
@@ -66,6 +66,8 @@
 	}
 </script>
 
+<!-- svelte-ignore a11y_click_events_have_key_events -->
+<!-- svelte-ignore a11y_no_static_element_interactions -->
 <div class="appt-wrapper" class:is-past={isPast} class:expanded={isExpanded} onclick={handleToggle}>
 	<div class="appt-card">
 		{#if showServiceColor}

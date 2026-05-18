405903494198aea14e2e4c247ab216316d7fe9b1
Lucas Brito <lucasbritoling@gmail.com>
2026-05-18 01:26:43 -0300

    Atualizar o agenda-strip.svelte

--- src/lib/components/app/agenda/agenda-strip.svelte
+++ src/lib/components/app/agenda/agenda-strip.svelte
@@ -35,7 +35,7 @@
 	{#each strip as day}
 		<button
 			onclick={() => onSelect(day.str)}
-			class="flex aspect-square h-17 w-16.5 shrink-0 flex-col items-center justify-center rounded-full border transition-all active:scale-95
+			class="flex aspect-square h-17 w-16.5 shrink-0 flex-col items-center justify-center rounded-md border transition-all active:scale-95
             {day.str === selectedDate
 				? 'border-foreground opacity-100'
 				: 'border-foreground/10 opacity-60'}"

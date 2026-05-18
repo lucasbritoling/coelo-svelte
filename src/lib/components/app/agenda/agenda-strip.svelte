2e9a3d2ede26d77c942b3f34fdb9e3f1ce46ead2
Lucas Ling <lucasbritoling@gmail.com>
2026-05-18 17:24:00 -0300

    minor

--- src/lib/components/app/agenda/agenda-strip.svelte
+++ src/lib/components/app/agenda/agenda-strip.svelte
@@ -30,6 +30,7 @@
 	});
 </script>
 
+<!-- svelte-ignore a11y_no_static_element_interactions -->
 <div
 	class="no-scrollbar flex snap-x snap-mandatory gap-2.5 overflow-x-auto scroll-smooth px-5 py-3.5"
 	ontouchstart={(e) => e.stopPropagation()}
